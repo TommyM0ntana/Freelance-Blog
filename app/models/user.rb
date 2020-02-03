faeab2d502b9ec629bfb6897ec2f5aa68204865e
ChristianOtieno <cgao273@gmail.com>
2020-02-04 00:05:26 +0300

    [Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_save { self.email = email.downcase }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Validation for user tests
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
