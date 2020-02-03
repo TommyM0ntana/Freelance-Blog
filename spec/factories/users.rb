34b241897c4ea90b55d3d2c526bb2b41cfc31c91
ChristianOtieno <cgao273@gmail.com>
2020-02-03 23:29:41 +0300

    [Users & Posts] - modified Faker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,5 @@
 FactoryBot.define do
   factory :user do
-    email { Faker::Internet.email }
-    username Faker::Name.first_name.downcase
-    password 'Sample:1'
-    password_confirmation 'Sample:1'
-    first_name Faker::Name.first_name
-    last_name Faker::Name.last_name
-    active true
-    confirmed true
+    
   end
 end
