1b84c7b16678bdede7cdf4aae9cfe58db3088822
ChristianOtieno <cgao273@gmail.com>
2020-02-04 00:25:21 +0300

    [Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'posts/index,'
+  get 'posts/new'
+  get 'posts/show'
+  get 'posts/create'
+  get 'users/index'
+  get 'users/new'
+  get 'users/show'
+  get 'users/create'
   devise_for :users
 end
