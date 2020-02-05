7f38e6f6090466ce63a40c5597342a5f09ac057e
ChristianOtieno <cgao273@gmail.com>
2020-02-05 23:44:11 +0300

    [Comments & Likes] - Add comments and likes controller

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validates :content, presence: true, length: { maximum: 280 }
+  validates :content, presence: true, length: { maximum: 280,
+                                                too_long: '280 characters in comment is the maximum allowed.' }
   validates :user_id, :post_id, presence: true
 end
