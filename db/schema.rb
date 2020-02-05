9498d0d3e5e20a97ec0ba741a77d5ab96e464497
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-02-05 22:52:36 +0100

    [Comments & Likes] - add create route

--- db/schema.rb
+++ db/schema.rb
@@ -37,9 +37,9 @@ ActiveRecord::Schema.define(version: 2020_02_05_202018) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -49,10 +49,16 @@ ActiveRecord::Schema.define(version: 2020_02_05_202018) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
