4cd00a6834552f0533aa4ac899f58988ec18b8b1
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-02-06 23:59:03 +0100

    [Friendships v1] -Add Gravatar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_162629) do
+ActiveRecord::Schema.define(version: 2020_02_06_153730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,12 +26,12 @@ ActiveRecord::Schema.define(version: 2020_02_06_162629) do
   end
 
   create_table "friendships", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "friend_id"
-    t.boolean "status"
+    t.bigint "user_id", null: false
+    t.bigint "friends_id", null: false
+    t.boolean "confirmed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["friends_id"], name: "index_friendships_on_friends_id"
     t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
@@ -47,9 +47,9 @@ ActiveRecord::Schema.define(version: 2020_02_06_162629) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -59,10 +59,17 @@ ActiveRecord::Schema.define(version: 2020_02_06_162629) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "friendships", "users"
+  add_foreign_key "posts", "users"
 end
