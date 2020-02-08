6afdb92abb1db0cdf6dc4250067d81c2c795c271
ChristianOtieno <cgao273@gmail.com>
2020-02-08 07:15:43 +0300

    [freiendships v1] - Add logic user and friend are different

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,4 +4,8 @@ class Friendship < ApplicationRecord
 
   validates :user, uniqueness: { scope: :friend }
   validate :user_and_friend_are_different
+
+  def user_and_friend_are_different
+    errors.add(:friend_id, "can't be same as user") if user_id == friend_id
+  end
 end
