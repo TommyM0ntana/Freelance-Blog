a3ab4c5a092da5cb05d9e5b39f7c767acb961680
ChristianOtieno <cgao273@gmail.com>
2020-02-12 15:17:27 +0300

    ADD more tests for likes

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,8 +2,4 @@ require 'rails_helper'
 require 'devise'
 
 RSpec.describe UsersController, type: :controller do
-  # it "should get index" do
-  #   get 'index'
-  #   response.should be_success
-  # end
 end
