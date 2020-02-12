db0e0d6151184e9e1d8acb8359ce8181b878f300
ChristianOtieno <cgao273@gmail.com>
2020-02-12 15:13:28 +0300

    ADD more tests for posts

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  before :each do
+    @user = User.create(email: 'user@mail.com', password: 'foobar')
+  end
+
   describe 'GET index' do
     it 'has a 302 status code' do
       get :index
@@ -20,5 +24,17 @@ RSpec.describe PostsController, type: :controller do
       get :new
       expect(response.status).to eq(302)
     end
+  end
+
+  describe 'Create new post' do
+    it 'creates a valid post' do
+      @post = @user.posts.build(title: 'title post', content: 'A new post')
+      expect(@post).to be_valid
+    end
+  end
+
+  it 'creates a blank post' do
+    @post = @user.posts.build(content: ' ')
+    expect(@post.valid?).to eql(false)
   end
 end
